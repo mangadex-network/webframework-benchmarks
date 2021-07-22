5fff08f3eb82f40896457aa25d8c1c918e35acbc
ronny1982 <wegener.ronny@gmail.com>
2021-07-22 10:11:40 +0200

    add go ssl servers

---
+++ servers/golang/servessl-fasthttp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/valyala/fasthttp"
+)
+
+var text = bytes.Repeat([]byte("X"), 8192)
+
+func ServeText(ctx *fasthttp.RequestCtx) {
+	//ctx.SetStatusCode(fasthttp.StatusOk)
+	ctx.SetBody(text)
+}
+
+func ServeImage(ctx *fasthttp.RequestCtx) {
+	fasthttp.ServeFile(ctx, "../../htdocs/sample.jpg")
+}
+
+func Router(ctx *fasthttp.RequestCtx) {
+	switch string(ctx.Path()) {
+	case "/sample.txt":
+		ServeText(ctx)
+	case "/sample.jpg":
+		ServeImage(ctx)
+	default:
+		ctx.Error("Not Found", fasthttp.StatusNotFound)
+	}
+}
+
+func main() {
+	fmt.Println("Server started on http://127.0.0.1:44300")
+	fasthttp.ListenAndServeTLS(":44300", "../../localhost.crt", "../../localhost.key", Router)
+}
